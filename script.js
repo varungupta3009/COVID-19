3975b81f3e38a85fcb84cba5a11678f23dee794a
Varun Gupta <varungupta3009@gmail.com>
2020-03-15 19:01:53 +0530

    Add Live Tag for Periodic Refresh

--- script.js
+++ script.js
@@ -1,45 +1,53 @@
-confirmed = "0";
-recovered = "-1";
-deceased = "-1";
+var scores = {
+  confirmed: "0",
+  recovered: "-1",
+  deceased: "-1",
+  getScores: () => {
+    Papa.parse(
+      "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv",
+      {
+        download: true,
+        dynamicTyping: true,
+        // header: true,
+        complete: function (results) {
+          scores.confirmed = results.data[16].pop();
+          document.querySelector(".confirmed").innerHTML = scores.confirmed;
+        }
+      }
+    );
 
-Papa.parse(
-  "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv",
-  {
-    download: true,
-    dynamicTyping: true,
-    // header: true,
-    complete: function (results) {
-      confirmed = results.data[16].pop();
-      document.querySelector(".confirmed").innerHTML = confirmed;
-    }
-  }
-);
+    Papa.parse(
+      "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv",
+      {
+        download: true,
+        dynamicTyping: true,
+        // header: true,
+        complete: function (results) {
+          scores.recovered = results.data[16].pop();
+          document.querySelector(".recovered").innerHTML = scores.recovered;
+        }
+      }
+    );
 
-Papa.parse(
-  "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv",
-  {
-    download: true,
-    dynamicTyping: true,
-    // header: true,
-    complete: function (results) {
-      recovered = results.data[16].pop();
-      document.querySelector(".recovered").innerHTML = recovered;
-    }
-  }
-);
+    Papa.parse(
+      "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv",
+      {
+        download: true,
+        dynamicTyping: true,
+        // header: true,
+        complete: function (results) {
+          scores.deceased = results.data[16].pop();
+          document.querySelector(".deceased").innerHTML = scores.deceased;
+        }
+      }
+    );
 
-Papa.parse(
-  "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv",
-  {
-    download: true,
-    dynamicTyping: true,
-    // header: true,
-    complete: function (results) {
-      deceased = results.data[16].pop();
-      document.querySelector(".deceased").innerHTML = deceased;
-    }
+    $(".tag").removeClass("is-black").addClass("is-danger")
   }
-);
+}
+
+scores.getScores()
+setInterval(scores.getScores, 600000)
 
 VANTA.CELLS({
   el: ".cells",
@@ -49,5 +57,5 @@ VANTA.CELLS({
   minWidth: 200.0,
   scale: 1.0,
   color1: 0x0,
-  color2: (confirmed > recovered) || (deceased > confirmed + recovered) ? 0xff0000 : 0x00ffff
+  color2: (scores.confirmed > scores.recovered) || (scores.deceased > scores.confirmed + scores.recovered) ? 0xff0000 : 0x00ffff
 });
